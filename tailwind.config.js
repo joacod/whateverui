6656c77edb08c4271e5ac6c9494e7d62a811be65
Joaquin Diaz <joacod@gmail.com>
2024-01-23 13:49:11 +0100

    body font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         display: ["Comfortaa Variable", "sans-serif"],
+        body: ["Exo Variable", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: "#687EFF",
